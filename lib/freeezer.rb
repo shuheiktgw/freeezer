8d683068ae658fcebe7642945d7c15773ae97dd3
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-02-24 16:50:14 +0900

    Implemented deep_freeze

--- lib/freeezer.rb
+++ lib/freeezer.rb
@@ -1,5 +1,27 @@
 require "freeezer/version"
 
 module Freeezer
-  # Your code goes here...
+  refine Hash do
+    def deep_freeze
+     self.freeze
+     self.each do |_key, value|
+       if value.is_a?(Array) || value.is_a?(Hash)
+         value.deep_freeze
+       end
+       value.freeze
+     end
+    end
+  end
+
+  refine Array do
+    def deep_freeze
+      self.freeze
+      self.each do |value|
+        if value.is_a?(Array) || value.is_a?(Hash)
+          value.deep_freeze
+        end
+        value.freeze
+      end
+    end
+  end
 end
